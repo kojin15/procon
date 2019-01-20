1ed5d1fda241e352ed75984479f23f71150c0995
kojin15 <kojin15@amigo2.ne.jp>
2019-01-20 21:44:03 +0900

    abc116

---
+++ atcoder/kotlin/abc_116/A.kt
@@ -0,0 +1,9 @@
+package kotlin.abc_116
+
+/**
+ * @author kojin15.
+ */
+fun main(args: Array<String>) {
+    val (AB, BC, CA) = readLine()!!.split(" ").map { it.toInt() }
+    println((AB * BC) / 2)
+}
---
+++ atcoder/kotlin/abc_116/B.kt
@@ -0,0 +1,23 @@
+package kotlin.abc_116
+
+/**
+ * @author kojin15.
+ */
+fun main(args: Array<String>) {
+    val s = readLine()!!.toInt()
+    val a = mutableListOf<Int>()
+    a.add(s)
+    var _a = s
+    for (i in 2..1000000) {
+        _a = f(_a)
+        if (a.contains(_a)) {
+            println(i)
+            return
+        }
+        a.add(_a)
+    }
+}
+
+fun f(n: Int): Int {
+    return if (n % 2 == 0) n / 2 else 3 * n + 1
+}
