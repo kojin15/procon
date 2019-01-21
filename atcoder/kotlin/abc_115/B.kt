d7c50aae597181aae1a627df01dbfa3dd6a7aab4
kojin15 <kojin15@amigo2.ne.jp>
2019-01-21 13:47:40 +0900

    abc115

---
+++ atcoder/kotlin/abc_115/B.kt
@@ -0,0 +1,14 @@
+package kotlin.abc_115
+
+/**
+ * @author kojin15.
+ */
+fun main(args: Array<String>) {
+    val N = readLine()!!.toInt()
+    val p = (1..N).map { readLine()!!.toInt() }.sortedDescending()
+    var S = p.first() / 2
+    for (i in 1 until N) {
+        S += p[i]
+    }
+    println(S)
+}
